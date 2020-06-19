af2c28394bc2275b50f5aba2c9a3fbc278dcae09
unknown <18234096788@sina.cn>
2020-06-19 16:02:01 +0800

    2020-6-19-1

---
+++ components/btn/btn.vue
@@ -0,0 +1,35 @@
+<template>
+	<view class="btns" :style="{color: color}" @click="onClick">
+		<slot></slot>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			color: {
+				type: String,
+				default: '#000',
+			}
+		},
+		data() {
+			return {
+				
+			};
+		},
+		methods: {
+			onClick() {
+				console.log('beijianji');
+				this.$emit('change', this.color);
+			}
+		}
+	}
+</script>
+
+<style>
+	.btns{
+		width: 100px;
+		height: 100px;
+		border: 1px red solid;
+	}
+</style>
---
+++ pages/about/about.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		guanyu
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
